264bc6b35fef95baed4c460d3dc1889f10ee54aa
prince nyarko <princenyarko360@gmail.com>
2025-10-17 21:16:08 +0000

    Modify task_5.sql

--- task_5.sql
+++ task_5.sql
@@ -3,5 +3,5 @@ INSERT INTO customer (
     customer_id = '1', 
     customer_name = 'Cole Baidoo', 
     email = 'cbaidoo@sandtech.com', 
-    address = '123 Hapiness Ave'
+    address = '123 Happiness Ave'
     );
