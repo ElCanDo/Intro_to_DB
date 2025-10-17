10e3e0f1e7617b878c19a40a92a390781fb6fc75
prince nyarko <princenyarko360@gmail.com>
2025-10-17 21:12:45 +0000

    Add task_5.sql

---
+++ task_5.sql
@@ -0,0 +1,7 @@
+USE alx_book_store;
+INSERT INTO Customers (
+    customer_id = '1', 
+    customer_name = 'Cole Baidoo', 
+    email = 'cbaidoo@sandtech.com', 
+    address = '123 Hapiness Ave'
+    );
